7dcbbeb40ccef282837fdf590e94c5688fee74fc
SaraBarrett <sara.c.mon@gmail.com>
2026-03-12 14:01:51 +0000

    exs

---
+++ js/events.js
@@ -0,0 +1,48 @@
+let helloBtn = document.getElementById("helloBtn");
+let loginBtn = document.getElementById("loginBtn");
+let divEvent = document.getElementById("divEvent");
+let myList = document.getElementById("list");
+let myForm = document.getElementById("form");
+
+function helloW() {
+  alert("hello mundo!");
+}
+
+function login() {
+  let pass = prompt("qual é a pass?");
+}
+
+function changeToGreen() {
+  this.style.backgroundColor = "green";
+}
+
+helloBtn.addEventListener("mouseout", helloW);
+loginBtn.addEventListener("click", login, { once: true });
+loginBtn.addEventListener("click", helloW);
+divEvent.addEventListener("click", changeToGreen);
+loginBtn.addEventListener("click", changeToGreen);
+helloBtn.addEventListener("click", changeToGreen);
+
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (event.key == "l" || event.key == "L") {
+    alert("evento de teclado!" + event.key);
+  }
+});
+
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //traz um objecto com TODOS os inputs do formulário
+  let data = new FormData(this);
+
+  //acede ao input do formulário que tem no name newAnimal
+  let animal = data.get("newAnimal");
+
+  let newLi = document.createElement("li");
+  newLi.innerText = animal;
+
+  myList.appendChild(newLi);
+
+  this.reset();
+
+});
--- js/exs.js
+++ js/exs.js
@@ -1,61 +1,93 @@
-//pp 9
-let imagem = document.getElementById("unicorn");
-let heading = document.getElementById("mainheading");
+// //pp 9
+// let imagem = document.getElementById("unicorn");
+// let heading = document.getElementById("mainheading");
 
-//pp12
-let doneTodos = document.querySelectorAll(".done");
-let input = document.querySelector("input[type='checkbox']");
+// //pp12
+// let doneTodos = document.querySelectorAll(".done");
+// let input = document.querySelector("input[type='checkbox']");
 
-// console.log(input);
-// console.log(doneTodos);
+// // console.log(input);
+// // console.log(doneTodos);
 
-//pp 15
-let myPickles = document.querySelector("span");
-console.log(myPickles);
+// //pp 15
+// let myPickles = document.querySelector("span");
+// console.log(myPickles);
 
-function changeTaste() {
-  if (myPickles.innerText == "Yammi") {
-    myPickles.innerText = "Yack";
-  } else {
-    myPickles.innerText = "Yammi";
-  }
-}
+// function changeTaste() {
+//   if (myPickles.innerText == "Yammi") {
+//     myPickles.innerText = "Yack";
+//   } else {
+//     myPickles.innerText = "Yammi";
+//   }
+// }
 
-//pp20
-function eggOrChicken() {
-  let imageEgg = document.querySelector("img");
+// //pp20
+// function eggOrChicken() {
+//   let imageEgg = document.querySelector("img");
 
-  if (
-    imageEgg.getAttribute("src") ==
-    "https://devsprouthosting.com/images/egg.jpg"
-  ) {
-    imageEgg.setAttribute(
-      "src",
-      "https://devsprouthosting.com/images/chicken.jpg",
-    );
-  } else {
-    imageEgg.setAttribute("src", "https://devsprouthosting.com/images/egg.jpg");
-  }
-}
-function makeRandom() {
-  let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+//   if (
+//     imageEgg.getAttribute("src") ==
+//     "https://devsprouthosting.com/images/egg.jpg"
+//   ) {
+//     imageEgg.setAttribute(
+//       "src",
+//       "https://devsprouthosting.com/images/chicken.jpg",
+//     );
+//   } else {
+//     imageEgg.setAttribute("src", "https://devsprouthosting.com/images/egg.jpg");
+//   }
+// }
+// function makeRandom() {
+//   let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-  let mySpans = document.getElementsByTagName("span");
+//   let mySpans = document.getElementsByTagName("span");
 
-  let count = 0;
-  for (let element of mySpans) {
-    element.style.color = colors[count++];
-  }
-}
+//   let count = 0;
+//   for (let element of mySpans) {
+//     element.style.color = colors[count++];
+//   }
+// }
 
-function changeOrder() {
-  let myLis = document.querySelectorAll("li");
-  console.log(myLis);
-  for (let element of myLis) {
-    if (element.classList.contains("highlight")) {
-      element.classList.remove("highlight");
-    } else {
-      element.classList.add("highlight");
-    }
-  }
-}
+// function changeOrder() {
+//   let myLis = document.querySelectorAll("li");
+//   console.log(myLis);
+//   for (let element of myLis) {
+//     if (element.classList.contains("highlight")) {
+//       element.classList.remove("highlight");
+//     } else {
+//       element.classList.add("highlight");
+//     }
+//   }
+// }
+
+// //pp 39
+// function createBtns() {
+//   let myParent = document.getElementById("container");
+
+//   for (let i = 1; i < 101; i++) {
+//     let myBtn = document.createElement("button");
+//     myBtn.innerText = "btn" + i;
+//     myParent.appendChild(myBtn);
+//   }
+// }
+
+//pp5
+
+let groceryForm = document.getElementById('myShoppingList');
+let myUl = document.querySelector('ul');
+
+groceryForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  
+  let groceryData = new FormData(this);
+
+  
+
+  let newLi = document.createElement('li');
+  newLi.innerText = groceryData.get('qt') + ':'+groceryData.get('product');
+
+  myUl.appendChild(newLi);
+
+  this.reset();
+
+})
